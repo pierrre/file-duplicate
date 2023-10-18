dcb2b5c3cf91e128b9b4f78ecbb9e8b9d96835a2
Pierre Durand <pierredurand@gmail.com>
2023-10-18 18:19:56 +0200

    stop using assert extensions in tests

--- cmd/file-duplicate/main_test.go
+++ cmd/file-duplicate/main_test.go
@@ -10,16 +10,7 @@ import (
 	"testing"
 
 	"github.com/pierrre/assert"
-	"github.com/pierrre/assert/ext/pierrrecompare"
-	"github.com/pierrre/assert/ext/pierrreerrors"
-	"github.com/pierrre/assert/ext/pierrrepretty"
 )
-
-func init() {
-	pierrrecompare.Configure()
-	pierrrepretty.ConfigureDefault()
-	pierrreerrors.Configure()
-}
 
 func TestOK(t *testing.T) {
 	ctx := context.Background()
