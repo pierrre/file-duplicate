bbe9cc2e89b2a575a5b1055c08c38d87cd43ca7c
Pierre Durand <pierredurand@gmail.com>
2025-03-25 20:45:09 +0100

    golangci-lint: remove exclusions

--- file_duplicate.go
+++ file_duplicate.go
@@ -189,7 +189,7 @@ func hashFile(opts *options, fp *File) (string, error) {
 	if err != nil {
 		return "", errors.Wrap(err, "open")
 	}
-	defer f.Close()
+	defer f.Close() //nolint:errcheck
 	h := sha256.New()
 	_, err = io.Copy(h, f)
 	if err != nil {
